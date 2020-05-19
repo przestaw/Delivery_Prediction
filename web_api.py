4a8a08b758700b48b68aa39548297fbeafc5a6a6
przestaw <przemek.stawczyk@gmail.com>
2020-05-19 13:57:38 +0200

    smooll query update
    Signed-off-by: przestaw <przemek.stawczyk@gmail.com>

--- web_api.py
+++ web_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 import data_loader as data
 import numpy as np
 
@@ -19,12 +19,25 @@ def name():
     }), 200
 
 
-@app.route('/delivery_time', methods=['GET'])
+@app.route('/api/v1.0/delivery_time/', methods=['GET'])
 def delivery_time():
+    if not request.json:
+        abort(400)
     query = np.array([np.nan, np.nan, np.nan, np.nan, np.nan])
 
-    return jsonify({
+    # TODO : check, nan or error if missing
+    query[0] = request.json['delivery_company']
+    query[1] = le_city.transform(request.json['city'])
+    query[2] = request.json['price']
+    # optional?
+    query[3] = le_cat.transform(request.json['category'])
+    query[4] = le_subcat.transform(request.json['subcategory'])
 
+    # TODO : modeling
+    # prediction =  model.predict(query.reshape(1, -1))
+
+    return jsonify({
+        'prediction': 0.,
         'status': 'not yet implemented'
     }), 200
 

445406c99452d88ce14bc0a1cf3cabf4fc9b29fb
przestaw <przemek.stawczyk@gmail.com>
2020-06-01 15:30:59 +0200

    tried RandomForrest - [too time consuming - on PrzeStaw machine]
    Signed-off-by: przestaw <przemek.stawczyk@gmail.com>

--- models_training.py
+++ models_training.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import xgboost as xgb
-from sklearn.metrics import max_error, mean_absolute_error, mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import max_error, mean_absolute_error, mean_squared_error, auc
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, KFold
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
@@ -17,31 +18,45 @@ def train_model(target, data, model_type='xgb', random_seed=42, randomized=True)
         params = {
             'objective': ['reg:squarederror'],
             'eval_metric': ['rmse'],
-            'learning_rate': [0.03, 0.05, 0.07],  # so called `eta` value
-            'max_depth': [4, 5, 6, 7],
-            'min_child_weight': [4],
+            'learning_rate': [0.03, 0.05, 0.07, 0.12],  # so called `eta` value
+            'max_depth': [3, 4, 5, 6, 7, 8, 9],
+            'min_child_weight': [1, 2, 4, 7],
             'silent': [1],
-            'n_estimators': [200, 320, 450, 600],
+            'n_estimators': [100, 200, 320, 450],
             'seed': [random_seed]
         }
-        n_iter = 28
+        n_iter = 256  # ~half of possible combinations
     elif model_type == 'tree':
         model = DecisionTreeRegressor()
         params = {
             'max_depth': [3, 4, 5, 6, 7, 8, 9],
             'random_state': [random_seed],
             'criterion': ['mse', 'friedman_mse', 'mae'],
-            'splitter': ['best', 'random']
+            'splitter': ['best', 'random'],
+            'min_samples_leaf': [1, 2, 4],
+            'min_samples_split': [2, 5, 10],
         }
-        n_iter = 22  # ~half of possible combinations
+        n_iter = 190  # ~half of possible combinations
     elif model_type == 'knn':
         model = KNeighborsRegressor()
         params = {
-            'n_neighbors': [3, 4, 5, 6, 8, 12, 15, 18, 22],
+            'n_neighbors': [3, 4, 5, 6, 8, 12, 15, 18, 22, 27, 32],
             'weights': ['uniform', 'distance'],
             'algorithm': ['ball_tree', 'kd_tree'],
         }
-        n_iter = 20  # ~half of possible combinations
+        n_iter = 22  # ~half of possible combinations
+    elif model_type == 'forrest':
+        model = RandomForestRegressor()
+        params = {
+            'n_estimators': [100, 200, 320, 450],
+            'max_depth': [3, 4, 5, 6, 7, 8, 9],
+            'random_state': [random_seed],
+            'criterion': ['mse', 'mae'],
+            'min_samples_leaf': [1, 2, 4],
+            'min_samples_split': [2, 5, 10],
+            'n_jobs': [-1]
+        }
+        n_iter = 252  # ~half of possible combinations
     else:
         return NotImplemented
 
@@ -62,8 +77,9 @@ def calc_metrics(predictions, test_y):
     err = max_error(test_y, predictions)
     avg = mean_absolute_error(test_y, predictions)
     avg_sqr = mean_squared_error(test_y, predictions)
+    auc_score = auc(test_y, predictions)
 
-    return [err, avg, avg_sqr]
+    return [err, avg, avg_sqr, auc_score]
 
 
 def compare_models(models, test_X, test_y):
@@ -75,10 +91,10 @@ def compare_models(models, test_X, test_y):
     for mdl_pred in test_predictions:
         err.append(calc_metrics(mdl_pred, test_y))
 
-    error = pd.DataFrame.from_records(err, columns=['max error', 'avg error', 'avg sqr error'],
+    error = pd.DataFrame.from_records(err, columns=['max error', 'avg error', 'avg sqr error', 'area under curve'],
                                       index=['mdl-' + str(index + 1) for index in range(len(models))])
     ret = pd.DataFrame.from_records(test_X)
-    ret['target'] = test_y
+    ret['target'] = np.asarray(test_y)
     for index, mdl in enumerate(models):
         ret['mdl-' + str(index + 1)] = test_predictions[index]
 
